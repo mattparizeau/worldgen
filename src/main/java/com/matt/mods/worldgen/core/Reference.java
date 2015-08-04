151be89c9d92d301961e36a7d5334876dd9e9545
Matthieu Parizeau <mattparizeau@hotmail.com>
2015-08-04 12:25:21 -0400

    Main Mod File Started

---
+++ src/main/java/com/matt/mods/worldgen/core/Reference.java
@@ -0,0 +1,9 @@
+package com.matt.mods.worldgen.core;
+
+public class Reference {
+	
+	public static final String modid = "mattworldgen";
+	public static final String modname = "WorldGen";
+	public static final String version = "1.0";
+	
+}
